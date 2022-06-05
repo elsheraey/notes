9212c4f2fa536ca4fb8a63449940a9ea0f531f27
Mourad ElSheraey <sherieymourad1997@gmail.com>
2022-06-05 12:01:08 +0200

    sign in logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -40,7 +40,25 @@ export class AuthService {
     }
   }
 
-  signin() {
-    return { msg: "I am signed in" };
+  async signin(dto: AuthDTO) {
+    // find the user by email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    // if user doesn't exist throw exception
+    if (!user) throw new ForbiddenException("Credentials incorrect");
+
+    // compare password
+    const pwMatches = await argon.verify(user.hash, dto.password);
+
+    // if password incorrect throw exception
+    if (!pwMatches) throw new ForbiddenException("Credentials incorrect");
+
+    // send back the user
+    delete user.hash
+    return user;
   }
 }
