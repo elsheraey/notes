70ca1a4f474601133f4963bdbafe7c6d800ae0f3
Mourad ElSheraey <sherieymourad1997@gmail.com>
2022-06-07 03:55:04 +0200

    auth e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,6 +3,8 @@ import { NestApplication } from '@nestjs/core';
 import { Test } from '@nestjs/testing';
 import { PrismaService } from '../src/prisma/prisma.service';
 import { AppModule } from '../src/app.module';
+import * as pactum from 'pactum';
+import { AuthDTO } from 'src/auth/dto/auth.dto';
 
 let app: NestApplication;
 let prisma: PrismaService;
@@ -16,14 +18,80 @@ describe('App e2e', () => {
     app = moduleRef.createNestApplication();
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     await app.init();
+    await app.listen(3333);
 
     prisma = app.get(PrismaService);
     await prisma.cleanDB();
+
+    pactum.request.setBaseUrl('http://localhost:3333/');
   });
 
   afterAll(() => {
     app.close();
   });
 
-  it.todo('test');
+  describe('Auth', () => {
+    const dto: AuthDTO = {
+      email: 'mourad@gmail.com',
+      password: '123',
+    };
+
+    describe('Signup', () => {
+      it('should throw if email empty', () => {
+        return pactum
+          .spec()
+          .post('auth/signup')
+          .withBody({
+            password: dto.password,
+          })
+          .expectStatus(400);
+      });
+
+      it('should throw if password empty', () => {
+        return pactum
+          .spec()
+          .post('auth/signup')
+          .withBody({
+            email: dto.email,
+          })
+          .expectStatus(400);
+      });
+
+      it('should throw if no body is provided', () => {
+        return pactum.spec().post('auth/signup').expectStatus(400);
+      });
+
+      it('should signup', () => {
+        return pactum
+          .spec()
+          .post('auth/signup')
+          .withBody(dto)
+          .expectStatus(201);
+      });
+    });
+
+    describe('Signin', () => {
+      // You can do the same for signin
+
+      it('should sign', () => {
+        return pactum
+          .spec()
+          .post('auth/signin')
+          .withBody(dto)
+          .expectStatus(200);
+      });
+    });
+  });
+
+  describe('User', () => {
+    describe('Get me', () => {});
+    describe('Edit user', () => {});
+  });
+
+  describe('Bookmarks', () => {
+    describe('Create bookmarks', () => {});
+    describe('Get bookmarks', () => {});
+    describe('Get bookmarks by ID', () => {});
+    describe('Delete bookmark', () => {});
+  });
 });
