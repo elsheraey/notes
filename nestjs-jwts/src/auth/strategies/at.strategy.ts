45fe524ed131de59e04bfb5792b32f54fc158fe6
Mourad ElSheraey <sherieymourad1997@gmail.com>
2022-06-09 00:54:31 +0200

    fix ExtractJWT.fromAuthHeaderAsBearerToken

--- nestjs-jwts/src/auth/strategies/at.strategy.ts
+++ nestjs-jwts/src/auth/strategies/at.strategy.ts
@@ -2,17 +2,22 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
+type JwtPayload = {
+  sub: string;
+  email: string;
+};
+
 @Injectable()
 export class AtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken,
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       // ignoreExpiration: false,
       secretOrKey: 'at-secret',
     });
   }
 
-  validate(payload: any) {
+  validate(payload: JwtPayload) {
     return payload;
   }
 }
